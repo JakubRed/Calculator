05c005ade9b6958f7c9dbebaebddbe3e029a664c
JakubRed <59733762+JakubRed@users.noreply.github.com>
2023-11-02 21:39:04 +0100

    add actual code
    vs project

---
+++ KalkulatorKS/Backend.cs
@@ -0,0 +1,124 @@
+﻿namespace KalkulatorKS
+{
+    static class Backend
+    {
+        static double result;
+        static string number = "";
+        static double formerResult;
+        static char equationOperator;
+        static bool formerResultEmpty = true;
+
+
+        static public void enterDigit(string digit) //or comma
+        {
+            number += digit;
+        }
+        static public string showNumber()
+        {
+            return number;
+        }        static public double showFormerResult()
+        {
+            return formerResult;
+        }
+        static public char showOperator()
+        {
+            return equationOperator;
+        }        static public double showResult()
+        {
+            return result;
+        }
+        static public bool isItFirstOperation() 
+        {
+            return formerResultEmpty;
+        }
+        static public bool isCommaInNumber()
+        {
+            return number.Contains(",");
+        }  
+        static public bool isNumberEntered()
+        {
+            return !number.Equals("");
+        }
+        static public void shortenTheNumber()
+        {
+            number = number.Remove(number.Length - 1);
+        }
+        static public void oppositeNumber()
+        {
+            number = "-" + number;
+        }
+        static public void cyrcleOfLIfe()
+        {
+            formerResult = result;
+            //result = 0;
+            number = "";
+        }
+        static public bool enterOperator(char enteredOperator)
+        {
+            bool resultChanged = false;
+            equationOperator = enteredOperator;
+            if (formerResultEmpty)
+            {
+                formerResult = Convert.ToDouble(number);
+                number = "";
+                resultChanged = true;   
+                formerResultEmpty = false;    
+            }
+
+            return resultChanged;
+        }
+        static public string currentDisplay() //Not complete
+        {
+            return number;
+        }
+        static public void tabulaRasa(/*bool includeResult*/)
+        {
+            //if (includeResult)
+            //{
+            //    result = 0;
+            //}
+            formerResult = 0;
+            formerResultEmpty = true;
+            number = "";
+            equationOperator = '\0';
+        }
+        static public int doTheMath()
+        {
+        int errorCode = 0; //by default set to success
+            switch (equationOperator)
+            {
+                case '+':
+                    result = formerResult + Convert.ToDouble(number);
+                break;
+                case '-':
+                    result = formerResult - Convert.ToDouble(number); ;
+                break;
+                case '/':
+                    if (0 == Convert.ToDouble(number))
+                    {
+                        errorCode = -2; // Attempt to divide by 0
+                    }
+                    else
+                    {
+                        result = formerResult / Convert.ToDouble(number);
+                    }
+                break;
+                case 'x':
+                    result = formerResult * Convert.ToDouble(number);
+                    break;
+                case '^':
+                    result = Math.Pow(formerResult, Convert.ToDouble(number));
+                break;
+                default:
+                    errorCode = -1; //unknown error
+                break;
+            }
+            return errorCode;
+        }
+        static public void cyrcleOfLife()
+        {
+            formerResult = result;
+            result = Convert.ToDouble(number);
+        }
+    }
+}
---
+++ KalkulatorKS/Frontend.cs
@@ -0,0 +1,215 @@
+namespace KalkulatorKS
+{
+    public partial class Frontend : Form
+    {
+        public Frontend()
+        {
+            InitializeComponent();
+        }
+        #region Digit buttons
+        private void Button0_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("0");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("1");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("2");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("3");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("4");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("5");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("6");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("7");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("8");
+            Display.Text = Backend.currentDisplay();
+        }
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            Backend.enterDigit("9");
+            Display.Text = Backend.currentDisplay();
+        }
+        #endregion Digit buttons
+
+        #region Operators buttons
+        private void ButtonAddition_Click(object sender, EventArgs e)
+        {
+            if (Backend.enterOperator('+')) //assiogn operator
+            {   //the first operation
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator());
+            }
+            else // not the first operation
+            {
+                //if (0 != Backend.doTheMath())
+                //{
+                //    //error
+                //}
+                //else
+                //{
+                    ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator()) /*+ " " + Backend.showNumber() + " ="*/;
+                    //ResultDisplay.Text += " " + Backend.showNumber() + " =";
+                    Display.Text = Convert.ToString(Backend.showResult());
+                //}
+
+            }
+        }
+        private void ButtonSubtraction_Click(object sender, EventArgs e)
+        {
+            if (Backend.enterOperator('-')) //assiogn operator
+            {   //the first operation
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator());
+            }
+            else // not the first operation
+            {
+     
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator()) /*+ " " + Backend.showNumber() + " ="*/;
+                Display.Text = Convert.ToString(Backend.showResult());
+            }
+        }
+        private void ButtonDivision_Click(object sender, EventArgs e)
+        {
+            if (Backend.enterOperator('/')) //assiogn operator
+            {   //the first operation
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator());
+            }
+            else // not the first operation
+            {
+
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator()) /*+ " " + Backend.showNumber() + " ="*/;
+                Display.Text = Convert.ToString(Backend.showResult());
+            }
+        }
+        private void ButtonMultiplication_Click(object sender, EventArgs e)
+        {
+            if (Backend.enterOperator('x')) //assiogn operator
+            {   //the first operation
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator());
+            }
+            else // not the first operation
+            {
+
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator()) /*+ " " + Backend.showNumber() + " ="*/;
+                Display.Text = Convert.ToString(Backend.showResult());
+            }
+        }
+        private void ButtonExponentiation_Click(object sender, EventArgs e)
+        {
+            if (Backend.enterOperator('^')) //assiogn operator
+            {   //the first operation
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator());
+            }
+            else // not the first operation
+            {
+
+                ResultDisplay.Text = Convert.ToString(Backend.showFormerResult()) + " " + Char.ToString(Backend.showOperator()) /*+ " " + Backend.showNumber() + " ="*/;
+                Display.Text = Convert.ToString(Backend.showResult());
+            }
+        }
+
+        #endregion Operators buttons
+
+        #region Other buttons
+        private void ButtonComma_Click(object sender, EventArgs e)
+        {
+
+            if (!Backend.isCommaInNumber())
+            {
+                Backend.enterDigit(",");
+                Display.Text = Backend.currentDisplay();
+            }
+        }
+        private void ButtonNegation_Click(object sender, EventArgs e)
+        {
+            Backend.oppositeNumber();
+            Display.Text = Backend.currentDisplay();
+        }
+        private void ButtonEqual_Click(object sender, EventArgs e)
+        {
+            if (!Backend.isItFirstOperation() && Backend.isNumberEntered())
+            {
+                ResultDisplay.Text = Backend.showFormerResult() + " " + Backend.showOperator() + " " + Convert.ToString(Backend.showNumber()) + " =";
+
+                switch (Backend.doTheMath())
+                {
+                    case 0:
+                        Display.Text = Convert.ToString(Backend.showResult());
+                        Backend.cyrcleOfLIfe();
+                        break;
+                    case -1:
+                        //add sth
+                        break;
+                    case -2:
+                        Display.Text = "Nie można dzielić przez 0";
+                        break;
+                    default:
+                        //add sth
+                        break;
+                }
+            }
+        }
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            if (Backend.isNumberEntered())
+            {
+                Backend.shortenTheNumber();
+
+            }
+            else
+            {
+                Backend.tabulaRasa();
+                ResultDisplay.Text = "";
+            }
+            Display.Text = Backend.currentDisplay();
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            Backend.tabulaRasa();
+            Display.Text = Backend.currentDisplay();
+            ResultDisplay.Text = Char.ToString((Backend.showOperator()));
+
+        }
+        #endregion Other buttons
+        private void Skin_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Operator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clock_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
